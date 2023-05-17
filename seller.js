0aa964e8c29808a1c7e2dc10f28abe340de57e36
remyacode <remyachandrasekhar710@gmail.com>
2023-05-17 11:30:52 +0530

    crud project 1

---
+++ seller.js
@@ -0,0 +1,100 @@
+
+var form = document.getElementById('my-form');
+var itemList = document.getElementById('items');
+var total=document.getElementById('tot');
+var count=0;
+//var editId = document.getElementById('editId');
+// Form submit event
+btn=document.querySelector('#my-form .btn');
+btn.addEventListener('click', addItem);
+
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+
+let crudid = '1a3604e2cda74339b8805da5974745ec';
+
+window.onload = () => {
+   
+    axios.get(`https://crudcrud.com/api/${crudid}/seller`)
+        .then((res) => {
+            res.data.forEach(e => {
+                
+                showOutput(e)
+            });
+        })
+}
+// Add item
+function addItem(e) {
+    e.preventDefault();
+    let newPrice = document.getElementById('price').value;
+    let newName = document.getElementById('name').value;
+    
+
+    class Myobj {
+        constructor(price,name) {
+            this.price = price;
+            this.name = name;
+            //count=count+this.price;
+        }
+    }
+    let postObj = new Myobj(newPrice, newName)
+    /*
+    if (editId && editId.textContent) {
+        axios.put(`https://crudcrud.com/api/${crudid}/seller/${editId.textContent}`, postObj)
+        showOutput(postObj)
+
+    } else { }
+        */
+        axios.post(`https://crudcrud.com/api/${crudid}/seller`, postObj)
+            .then(res => showOutput(res.data))
+   
+    form.reset()
+}
+// Remove item
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+
+        if (confirm('Are You Sure?')) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+            var p=li.innerText.split('-');
+            count=count-p[0];
+            total.innerHTML=`Total value of products: Rs.${count}`;
+            let editobjkey = li.classList[1];
+            
+
+            axios.delete(`https://crudcrud.com/api/${crudid}/seller/${editobjkey}`)
+        }
+    }
+}
+
+    
+    function showOutput(data) {
+        itemList.insertAdjacentHTML("beforeend", `
+            <li class="list-group-item ${data._id}">${data.price}-${data.name}<button class="btn btn-danger btn-sm float-right delete">Delete</button></li>`);
+        count+=parseInt(data.price)
+            total.innerHTML=`Total value of products: Rs.${count}`;
+    
+    }
+
+
+/*
+// edit item
+function editItem(e) {
+    
+
+    if (e.target.classList.contains('edit')) {
+        var li = e.target.parentElement;
+        let editData = li.innerText.split("-");
+        let editobjkey = li.classList[1];
+        editId.textContent = editobjkey;
+        //console.log(editData)
+        document.getElementById('name').value = editData[0];
+        document.getElementById('femail').value = editData[1];
+        document.getElementById('phone').value = editData[2].split(0,10)[0];
+        itemList.removeChild(li);
+    
+    }
+}
+*/
